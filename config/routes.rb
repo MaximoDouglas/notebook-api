22528d37fb9d6ab27f683d72e44104f53f6f0e7e
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-08-07 02:31:25 -0300

    Add relationships/kind to the contacts route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   resources :contacts do
     resource :kind, only: [:show]
+    resource :kind, only: [:show], path: 'relationships/kind'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
