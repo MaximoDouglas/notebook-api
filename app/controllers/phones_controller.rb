8fa93c58f99d82efc0bdd53ed0870ef5ac719637
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-08-07 03:10:55 -0300

    Clean phones_controller

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -8,11 +8,6 @@ class PhonesController < ApplicationController
 
     private
         def set_phones
-            if params[:contact_id]
-                @phones = Contact.find(params[:contact_id]).phones
-                return @phones
-            end
-
-            @phones = phones.where(id: params[:id])
+            @phones = Contact.find(params[:contact_id]).phones    
         end
 end
