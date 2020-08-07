9d4e9bdc642105efe91fff9c523b27980e1da159
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-08-07 03:03:36 -0300

    Enable to get phones by contact id

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,11 +1,14 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
   
-  has_many :phones
   has_one  :address
 
   belongs_to :kind do 
     link(:related) { contact_kind_url(object.id) }
+  end
+
+  has_many :phones do
+    link(:related) { contact_phones_url(object.id) }
   end
 
   meta do 
